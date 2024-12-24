e04cac0b7234a64d2d9c146c258e2ce71295d9ae
Freddy <freddy990117@gmail.com>
2024-12-25 07:00:46 +0800

    Initial Setup

--- Layout.jsx
+++ Layout.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 
-const Layout = ({ Component }) => {
-  return (
-    <div>
-      <h2>I am in Layout</h2>
-
-      <div className="l-header"></div>
-      <div className="l-main"></div>
-      <div className="l-footer"></div>
-    </div>
-  );
+const Layout = () => {
+  return;
 };
 
 export default Layout;
