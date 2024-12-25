ba63c8a0ef48830d2d148e7b7b7152eb7c1eccf8
Freddy <freddy990117@gmail.com>
2024-12-25 09:45:49 +0800

    完成 Header 設定，開始設定 CSS

--- Layout.jsx
+++ Layout.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+const Header = () => {
+  return (
+    <div className="l-header">
+      <h2>Freddy</h2>
+      <div className="navBar">
+        {navs.map((nav) => (
+          <div className="nav">{nav.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const navs = [
+  { name: "Home" },
+  { name: "About" },
+  { name: "Skill" },
+  { name: "Project" },
+  { name: "Contact" },
+];
 const Layout = () => {
-  return;
+  return (
+    <div>
+      <Header />
+      <div className="l-main"></div>
+      <div className="l-footer"></div>
+    </div>
+  );
 };
 
 export default Layout;
