0179ec44f082c2e84d45825e991a36f09fe8ee16
Freddy <freddy990117@gmail.com>
2024-12-31 07:03:02 +0800

    Add Pages Info

---
+++ src/Pages/Project.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Project = () => {
+  return <div>This is Project Page</div>;
+};
+
+export default Project;
---
+++ src/Pages/Skill.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Skill = () => {
+  return <div>This is skill Page</div>;
+};
+
+export default Skill;
