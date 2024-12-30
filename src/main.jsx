45ba6ac36c28e7354d6ab898e87446230d539f46
Freddy <freddy990117@gmail.com>
2024-12-31 07:03:52 +0800

    Update Layout Path

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Layout from "../Layout.jsx";
+import Contact from "./Pages/Contact.jsx";
+import Project from "./Pages/Project.jsx";
+import Resume from "./Pages/Resume.jsx";
+import Skill from "./Pages/Skill.jsx";
+import Page404 from "./Pages/Page404.jsx";
 const router = createBrowserRouter([
   {
-    path: "*",
-    element: <Layout {...{}} />,
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "Contact", element: <Contact /> },
+      { path: "Project", element: <Project /> },
+      { path: "Resume", element: <Resume /> },
+      { path: "Skill", element: <Skill /> },
+      { path: "*", element: <Page404 /> },
+    ],
   },
 ]);
 
