87f50afb5b9a11d0006166ba56cd80f7cffb345a
Freddy <freddy990117@gmail.com>
2025-01-07 06:56:24 +0800

    Delete Skill.jsx path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import Layout from "../Layout.jsx";
 import Contact from "./Pages/Contact.jsx";
 import Project from "./Pages/Project.jsx";
 import Resume from "./Pages/Resume.jsx";
-import Skill from "./Pages/Skill.jsx";
 import Page404 from "./Pages/Page404.jsx";
 import Homepage from "./Pages/Homepage.jsx";
 
@@ -19,7 +18,6 @@ const router = createBrowserRouter([
       { path: "Contact", element: <Contact /> },
       { path: "Project", element: <Project /> },
       { path: "Resume", element: <Resume /> },
-      { path: "Skill", element: <Skill /> },
       { path: "*", element: <Page404 /> },
     ],
   },
