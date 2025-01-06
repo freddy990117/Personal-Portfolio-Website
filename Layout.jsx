4cbc380316c84111b0e07bfaaf205666c74c1a1a
Freddy <freddy990117@gmail.com>
2025-01-07 05:48:16 +0800

    Change Main

--- Layout.jsx
+++ Layout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useNavigate, Outlet } from "react-router-dom";
+import { useNavigate, Outlet, useLocation } from "react-router-dom";
 
 import "./Layout.css";
 const navs = [
   { name: "Resume", path: "/Resume" },
-  { name: "Project", path: "/Project" },
-  { name: "Skill", path: "/Skill" },
-  { name: "Contact", path: "/Contact" },
+  { name: "Project", path: "Project" },
+  { name: "Skill", path: "Skill" },
+  { name: "Contact", path: "Contact" },
 ];
 
 const navFooter = [
@@ -43,7 +43,6 @@ const Header = () => {
             key={index}
             className="nav-header"
             onClick={() => navigate(nav.path)}
-            style={{ cursor: "pointer" }}
           >
             {nav.name}
           </div>
@@ -84,51 +83,57 @@ const Footer = () => {
     </div>
   );
 };
-const Main = () => {
-  const navigate = useNavigate();
 
-  return (
-    <div className="l-main">
-      <div className="main-info">
-        <div className="main-picture">
-          <img src="./Picture/Picture.png" />
-        </div>
-        <div className="main-aboutme">
-          <div className="info">
-            <h1>Hello</h1>
-            <h3>A Bit About Me</h3>
-            <p>
-              Hi, I'm Freddy, a front-end developer. I'm passionate about
-              developing web applications and always eager to learn new
-              technologies. I'm always looking for new challenges and
-              opportunities to grow.
-            </p>
-          </div>
+// 如果設定 Main 在 Layout.jsx 內，會導致所有 Component 都會顯示 Main 的內容，
+// 所以要建立一個 Homepage.jsx 來存放 Main 的內容
+// const Main = () => {
+//   const navigate = useNavigate();
 
-          <div className="navBar-main">
-            {navs.map((nav, index) => (
-              <div
-                key={index}
-                className="nav-main"
-                // 因為key 的 index 是專門用於識別列表中的每個子項，以優化 React 的渲染性能，但它不會自動轉換為 DOM 的屬性，
-                // 這就是為什麼無法直接用 key 的值來控制樣式。
-                data-index={index}
-                onClick={() => navigate(nav.path)}
-              >
-                {nav.name}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="l-main">
+//       <div className="main-info">
+//         <div className="main-picture">
+//           <img src="./Picture/Picture.png" />
+//         </div>
+//         <div className="main-aboutme">
+//           <div className="info">
+//             <h1>Hello</h1>
+//             <h3>A Bit About Me</h3>
+//             <p>
+//               Hi, I'm Freddy, a front-end developer. I'm passionate about
+//               developing web applications and always eager to learn new
+//               technologies. I'm always looking for new challenges and
+//               opportunities to grow.
+//             </p>
+//           </div>
+
+//           <div className="navBar-main">
+//             {navs.map((nav, index) => (
+//               <div
+//                 key={index}
+//                 className="nav-main"
+//                 // 因為key 的 index 是專門用於識別列表中的每個子項，以優化 React 的渲染性能，但它不會自動轉換為 DOM 的屬性，
+//                 // 這就是為什麼無法直接用 key 的值來控制樣式。
+//                 data-index={index}
+//                 onClick={() => navigate(nav.path)}
+//                 style={{ cursor: "pointer" }}
+//               >
+//                 {nav.name}
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 const Layout = () => {
   return (
     <div>
       <Header />
-      <Main />
+      {/* <Main /> */}
+      <Outlet />
       <Footer />
     </div>
   );
