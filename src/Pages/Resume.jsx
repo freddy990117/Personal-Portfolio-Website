3f305ad5256345bea6491f2fdd32191d4635732d
Freddy <freddy990117@gmail.com>
2025-01-08 07:06:22 +0800

    Update Resume Style (Not Finish)

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
+const Work_Experience = () => {
+  return (
+    <div className="resume-page">
+      <div className="resume-left">
+        <div className="resume-title">
+          <h1>Title</h1>
+        </div>
+        <div className="resume-points">
+          <h2>Work Experience</h2>
+        </div>
+      </div>
+      <div className="resume-right">
+        <div className="resume-time">
+          <h3>2022-2025</h3>
+        </div>
+        <div className="resume-content">
+          <h3>Company Name</h3>
+          <p>
+            I'm a paragraph. Click here to add your own text and edit me. It’s
+            easy. Just click “Edit Text” or double click me to add your own
+            content and make changes to the font.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Resume = () => {
-  return <div>This is Resume Page</div>;
+  return (
+    <div>
+      <Work_Experience />
+      <Work_Experience />
+      <Work_Experience />
+    </div>
+  );
 };
 
 export default Resume;
