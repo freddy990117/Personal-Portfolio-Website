18073ab4c4990dc78f75f63b689a98978e5994bb
Freddy <freddy990117@gmail.com>
2025-01-11 07:07:08 +0800

    Resume 模板-1

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -3,25 +3,25 @@ import React from "react";
 const Work_Experience = () => {
   return (
     <div className="resume-page">
-      <div className="resume-left">
-        <div className="resume-title">
-          <h1>Title</h1>
-        </div>
-        <div className="resume-points">
+      <div className="resume-title">
+        <h1>Resume</h1>
+      </div>
+      <div className="resume-info">
+        <div className="resume-left">
           <h2>Work Experience</h2>
         </div>
-      </div>
-      <div className="resume-right">
-        <div className="resume-time">
-          <h3>2022-2025</h3>
-        </div>
-        <div className="resume-content">
-          <h3>Company Name</h3>
-          <p>
-            I'm a paragraph. Click here to add your own text and edit me. It’s
-            easy. Just click “Edit Text” or double click me to add your own
-            content and make changes to the font.
-          </p>
+        <div className="resume-right">
+          <div className="resume-time">
+            <h3>2024 - Present</h3>
+          </div>
+          <div className="resume-job">
+            <h3>Software Developer</h3>
+            <p>
+              I'm a paragraph. Click here to add your own text and edit me. It’s
+              easy. Just click “Edit Text” or double click me to add your own
+              content and make changes to the font.
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -31,8 +31,6 @@ const Work_Experience = () => {
 const Resume = () => {
   return (
     <div>
-      <Work_Experience />
-      <Work_Experience />
       <Work_Experience />
     </div>
   );
