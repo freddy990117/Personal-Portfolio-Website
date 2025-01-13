103474e0ca9c201e163192f808562e7183c98a6c
Freddy <freddy990117@gmail.com>
2025-01-14 07:02:29 +0800

    新增專案資訊區塊，調整樣式與內容，包含專案名稱、描述及功能介紹

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -8,12 +8,20 @@ const Work_Experience = () => {
       {/* 用比較笨的方法加，思考應該可以用 Components 的方式用 */}
       <div className="project-info">
         <div className="project-left">
-          <h2>Project Name 1</h2>
+          <h2>Project 1 - Pexel Picture Search</h2>
+          <br />
+          <h3>
+            這個專案使用 React 撰寫，並使用 Axios 串接 Pexel API
+            來搜尋和顯示圖片。
+          </h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-            beatae non dolorum mollitia quaerat maiores harum impedit itaque
-            architecto hic quia earum quae porro amet, ab adipisci minus ut.
-            Ratione!
+            1.使用 BrowserRouter 來管理各個路由，讓用戶可以在不同頁面之間導航。
+            <br />
+            2.使用 map
+            方法來顯示圖片列表，並且每張圖片都可以點擊進入該圖片的詳細頁面。
+            <br />
+            3.這個專案展示了如何整合第三方 API 並在 React
+            應用中使用路由和狀態管理。
           </p>
         </div>
         <div className="project-right">
