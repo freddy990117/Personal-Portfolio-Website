07915cf727231ba9e51d626d70de8a0018296a16
Freddy <freddy990117@gmail.com>
2025-01-14 06:17:44 +0800

    Project 初版

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+const Work_Experience = () => {
+  return (
+    <div className="page">
+      <div className="title">
+        <h1>Project</h1>
+      </div>
+      {/* 用比較笨的方法加，思考應該可以用 Components 的方式用 */}
+      <div className="project-info">
+        <div className="project-left">
+          <h2>Project Name 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            beatae non dolorum mollitia quaerat maiores harum impedit itaque
+            architecto hic quia earum quae porro amet, ab adipisci minus ut.
+            Ratione!
+          </p>
+        </div>
+        <div className="project-right">
+          <img src="/Picture/Project-1.png" alt="Project-1-Picture" />
+        </div>
+      </div>
+      <div className="project-info">
+        <div className="project-left">
+          <h2>Project Name 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            beatae non dolorum mollitia quaerat maiores harum impedit itaque
+            architecto hic quia earum quae porro amet, ab adipisci minus ut.
+            Ratione!
+          </p>
+        </div>
+        <div className="project-right">
+          <img src="/Picture/Project-2.png" alt="Project-1-Picture" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Project = () => {
-  return <div>This is Project Page</div>;
+  return <Work_Experience />;
 };
 
 export default Project;
