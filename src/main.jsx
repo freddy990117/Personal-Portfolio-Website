b98bcf0cba6c90ab2afcdf250bc379e1b049ea0c
freddy <freddy990117@gmail.com>
2025-01-30 16:28:11 +0800

    更改 Router 使用 HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -9,20 +15,36 @@ import Resume from "./Pages/Resume.jsx";
 import Page404 from "./Pages/Page404.jsx";
 import Homepage from "./Pages/Homepage.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <Homepage /> },
-      { path: "Contact", element: <Contact /> },
-      { path: "Project", element: <Project /> },
-      { path: "Resume", element: <Resume /> },
-      { path: "*", element: <Page404 /> },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { path: "/", element: <Homepage /> },
+//       { path: "Contact", element: <Contact /> },
+//       { path: "Project", element: <Project /> },
+//       { path: "Resume", element: <Resume /> },
+//       { path: "*", element: <Page404 /> },
+//     ],
+//   },
+// ]);
 
+// createRoot(document.getElementById("root")).render(
+//   <RouterProvider router={router} />
+// );
+
+// 改成使用 HashRouter，因為它能避免由於 URL 路徑錯誤導致的 404 錯誤，
+// 並且能確保網站即使在刷新或深鏈接的情況下，也能正確加載。
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path="Contact" element={<Contact />} />
+        <Route path="Project" element={<Project />} />
+        <Route path="Resume" element={<Resume />} />
+        <Route path="*" element={<Page404 />} />
+      </Route>
+    </Routes>
+  </HashRouter>
 );
