12280958f24e8ca47d90a68b3bee17e30d8b31ff
freddy <freddy990117@gmail.com>
2025-01-30 16:28:48 +0800

    新增 base name

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   // 上傳檔案後的操作 : npm install gh-pages --save-dev   => 部屬的名稱 : gh-pages 只安裝在此資料夾 --save-dev
   // 再到 packjsoon 新增 scripts: "deploy": "vite build && gh-pages -d dist"
   // 最後 npm run deploy，即可將檔案上傳至 github
-  base: "/", // 這邊要是遠端名稱!!!!!
+  base: "/Personal-Portfolio-Website/", // 這邊要是遠端名稱!!!!!
   plugins: [react()],
 });
